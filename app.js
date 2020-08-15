9a51999e47ec34c701a49ef4198fca6c810aa23d
leogirardeau <leogir.pro@gmail.com>
2020-08-15 18:28:37 +0200

    selectors + furst button event

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 //Selectors
+const todoInput = document.querySelector('.todo-input');
+const todoButton = document.querySelector('.todo-button');
+const todoList = document.querySelector('.todo-list');
 
 //Event Listeners
+todoButton.addEventListener('click', addTodo);
 
 //Functions
+
+function addTodo(event) {
+
+};
