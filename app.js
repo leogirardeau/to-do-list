c5a624fc1b2cccf4561ed2cd731aaf0c1b6c24cd
leogirardeau <leogir.pro@gmail.com>
2020-08-15 19:38:54 +0200

    function

--- app.js
+++ app.js
@@ -9,5 +9,26 @@ todoButton.addEventListener('click', addTodo);
 //Functions
 
 function addTodo(event) {
-
+    //Prevent form from submitting
+    event.preventDefault();
+    //Todo DIV
+    const todoDiv = document.createElement('div');
+    todoDiv.classList.add('todo');
+    //Create LI
+    const newTodo = document.createElement('li');
+    newTodo.innerText = 'hey';
+    newTodo.classList.add('todo-item');
+    todoDiv.appendChild(newTodo);
+    //Check Mark Button
+    const completedButton = document.createElement('button');
+    completedButton.innerHTML = '<i class="fas fa-check"></i>';
+    completedButton.classList.add('complete-btn');
+    todoDiv.appendChild(completedButton);
+    //Check Trash Button
+    const trashButton = document.createElement('button');
+    trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+    trashButton.classList.add('complete-btn');
+    todoDiv.appendChild(trashButton);
+    //APPEND TO LIST
+    todoList.appendChild(todoDiv);
 };
