e097fc51468a6fcdceb1be0ed60789923bab093b
leogirardeau <leogir.pro@gmail.com>
2020-08-16 11:46:09 +0200

    delete and check button

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const todoList = document.querySelector('.todo-list');
 
 //Event Listeners
 todoButton.addEventListener('click', addTodo);
+todoList.addEventListener('click', deleteCheck);
 
 //Functions
 
@@ -34,3 +35,18 @@ function addTodo(event) {
     //Clear Todo INPUT VALUE
     todoInput.value = "";
 };
+
+function deleteCheck(e) {
+    const item = e.target;
+    //DELETE TODO
+    if (item.classList[0] === 'trash-btn') {
+        const todo = item.parentElement;
+        todo.remove();
+    }
+
+    //CHECK MARK
+    if (item.classList[0] === "complete-btn") {
+        const todo = item.parentElement;
+        todo.classList.toggle("completed");
+    }
+}
